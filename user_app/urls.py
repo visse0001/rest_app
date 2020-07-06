11de7aade4ac3c8c760acdd6bedbe3df46159b7e
Sandra_Kuczynska <san.kuczynska@gmail.com>
2020-07-06 22:25:20 +0200

    user profile

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from user_app.views import UserRegistrationView
+from user_app.views import UserRegistrationView, UserLoginView, UserProfileView
 
 urlpatterns = [
     path('signup', UserRegistrationView.as_view()),
+    path('signin', UserLoginView.as_view()),
+    path('profile', UserProfileView.as_view()),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,9 +1,12 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-from user_app.serializers import UserRegistrationSerializer
+from user_app.serializers import UserRegistrationSerializer, UserLoginSerializer
+from user_app.models import UserProfile
 
 
 class UserRegistrationView(CreateAPIView):
@@ -21,4 +24,54 @@ class UserRegistrationView(CreateAPIView):
             'message': 'User registered  successfully',
         }
 
+        return Response(response, status=status_code)
+
+
+class UserLoginView(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        response = {
+            'success': 'True',
+            'status code': status.HTTP_200_OK,
+            'message': 'User logged in successfully',
+            'token': serializer.data['token'],
+        }
+        status_code = status.HTTP_200_OK
+
+        return Response(response, status=status_code)
+
+
+class UserProfileView(RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_class = JSONWebTokenAuthentication
+
+    def get(self, request, **kwargs):
+        try:
+            user_profile = UserProfile.objects.get(user=request.user)
+            status_code = status.HTTP_200_OK
+            response = {
+                'success': 'true',
+                'status code': status_code,
+                'message': 'User profile fetched successfully',
+                'data': [{
+                    'first_name': user_profile.first_name,
+                    'last_name': user_profile.last_name,
+                    'phone_number': user_profile.phone_number,
+                    'age': user_profile.age,
+                    'gender': user_profile.gender,
+                }]
+            }
+
+        except Exception as e:
+            status_code = status.HTTP_400_BAD_REQUEST
+            response = {
+                'success': 'false',
+                'status code': status.HTTP_400_BAD_REQUEST,
+                'message': 'User does not exists',
+                'error': str(e)
+            }
         return Response(response, status=status_code)
